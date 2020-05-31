8695b9388bf5261edfa97b1b049928e2934a81ef
JDSNOTE <headstone1220@gmail.com>
2020-05-31 21:48:01 +0900

    05-31 SqlSession Bean edit

--- CADIO/src/test/java/com/cadio/jdbc/JDBCTest.java
+++ CADIO/src/test/java/com/cadio/jdbc/JDBCTest.java
@@ -1,5 +1,44 @@
 package com.cadio.jdbc;
 
-public class JDBCTest {
+import static org.junit.Assert.assertNotNull;
 
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/junit-context.xml"})
+public class JDBCTest {
+	
+	@Resource(name = "dataSource")
+	private DataSource ds;
+	@Resource(name = "sqlSessionFactory")
+	private SqlSessionFactory sf;
+	@Resource(name = "sqlSession")
+	private SqlSession sq;
+	
+	@Test
+	public void testDataSource() {
+		assertNotNull(ds); 
+	}
+	
+	@Test
+	public void testSqlSessionFactory() {
+		assertNotNull(sf);
+	}
+	
+	
+	@Test
+	public void testSqlSession() {
+		assertNotNull(sq);
+	}
+	
 }
